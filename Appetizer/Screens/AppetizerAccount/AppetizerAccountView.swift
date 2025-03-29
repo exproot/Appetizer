66a24608d4da5684e8776580cc3818873cb7c4f1
Ertan Yağmur <ertanyagmur@yahoo.com>
2025-03-29 16:27:52 +0300

    account view added

--- Appetizer/Screens/AppetizerAccount/AppetizerAccountView.swift
+++ Appetizer/Screens/AppetizerAccount/AppetizerAccountView.swift
@@ -8,11 +8,48 @@
 import SwiftUI
 
 struct AppetizerAccountView: View {
+
+  @StateObject private var viewModel = AppetizerAccountViewModel()
+
   var body: some View {
     NavigationView {
-      Text("Account")
-        .navigationTitle("Account")
+      Form {
+        Section {
+          TextField("First Name", text: $viewModel.firstName)
+          TextField("Last Name", text: $viewModel.lastName)
+          TextField("Email", text: $viewModel.email)
+            .keyboardType(.emailAddress)
+            .textInputAutocapitalization(.never)
+            .autocorrectionDisabled()
+
+          DatePicker("Birthdate", selection: $viewModel.birthdate, displayedComponents: .date)
+
+          Button {
+            viewModel.saveChanges()
+          } label: {
+            Text("Save Changes")
+          }
+        } header: {
+          Text("Personal Info")
+        }
+
+        Section {
+          Toggle("Extra Napkins", isOn: $viewModel.extraNapkins)
+          Toggle("Frequent Refills", isOn: $viewModel.frequentRefills)
+        } header: {
+          Text("Requests")
+        }
+//        .toggleStyle(SwitchToggleStyle(tint: Color.green))
+      }
+      .navigationTitle("Account")
     }
+    .alert(item: $viewModel.alertItem, content: { alertItem in
+      Alert(
+        title: alertItem.title,
+        message: alertItem.message,
+        dismissButton: alertItem.dismissButton
+      )
+    })
   }
 }
 
--- Appetizer/Utilities/Alert.swift
+++ Appetizer/Utilities/Alert.swift
@@ -16,6 +16,7 @@ struct AlertItem: Identifiable {
 
 struct AlertContext {
 
+  // MARK: Network Alerts
   static let invalidURL = AlertItem(
     title: Text("Something Went Wrong"),
     message: Text("An error ocurred while connecting to the server."),
@@ -37,6 +38,19 @@ struct AlertContext {
   static let unableToComplete = AlertItem(
     title: Text("Server Error"),
     message: Text("Unable to complete your request. Please check your internet connection."),
+    dismissButton: .default(Text("Ok"))
+  )
+
+  // MARK: Account Alerts
+  static let invalidForm = AlertItem(
+    title: Text("Invalid Form"),
+    message: Text("Please ensure all fields in the form have been filled out."),
+    dismissButton: .default(Text("Ok"))
+  )
+
+  static let invalidEmail = AlertItem(
+    title: Text("Invalid Email"),
+    message: Text("Please ensure your email is correct."),
     dismissButton: .default(Text("Ok"))
   )
 }
